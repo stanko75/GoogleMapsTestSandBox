dc0d03573ef8a49f860a1805ef933d34303c56a0
Stanko Milošev <stanko75@users.noreply.github.com>
2025-11-12 20:51:06 +0100

    endless load KML with retrofit and courutines

--- app/src/main/java/com/milosev/googlemapstestsandbox/LoadKml.kt
+++ app/src/main/java/com/milosev/googlemapstestsandbox/LoadKml.kt
@@ -2,46 +2,54 @@ package com.milosev.googlemapstestsandbox
 
 import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.maps.android.data.kml.KmlLayer
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
-import java.io.File
-import java.io.FileInputStream
 import java.net.URL
 
 class LoadKml {
-    fun execute(activity: Context, googleMap: GoogleMap) {
 
-        val kmlClient = CreateRetrofitBuilder().createRetrofitBuilder("https://www.milosev.com/")
+    private var kmlLayer: KmlLayer? = null
+
+    suspend fun execute(activity: Context, googleMap: GoogleMap, strUrl: String) {
+
+        val url = URL(strUrl)
+        val kmlClient = CreateRetrofitBuilder().createRetrofitBuilder("${url.protocol}://${url.host}/")
             .create(IGetKml::class.java)
 
-        CoroutineScope(Dispatchers.Main).launch {
+        withContext(Dispatchers.IO) {
+            while (isActive)
+            {
+                try {
+                    val webApiRequest =
+                        kmlClient.getKml(strUrl);
 
-            try {
-                val webApiRequest =
-                    kmlClient.getKml("https://www.milosev.com/gallery/allWithPics/travelBuddies/tunis/kml/kml.kml");
+                    if (webApiRequest.isSuccessful) {
+                        val bytes = webApiRequest.body()?.bytes()
+                        if (bytes != null) {
 
-                if (webApiRequest.isSuccessful) {
-                    val bytes = webApiRequest.body()?.bytes()
-                    if (bytes != null) {
-                        val input = ByteArrayInputStream(bytes)
-                        val kmlLayer = KmlLayer(googleMap, input, activity)
-                        kmlLayer.addLayerToMap()
-                        Log.i("KML", "Loaded successfully")
+                            val input = ByteArrayInputStream(bytes)
+
+                            withContext(Dispatchers.Main) {
+                                kmlLayer?.removeLayerFromMap()
+                                Log.i("KML", "Removed successfully")
+
+                                kmlLayer = KmlLayer(googleMap, input, activity)
+                                kmlLayer?.addLayerToMap()
+                                Log.i("KML", "Loaded successfully")
+                            }
+                        }
+                    } else {
+                        Log.e("KML", "Error: ${webApiRequest.code()}")
                     }
-                } else {
-                    Log.e("KML", "Error: ${webApiRequest.code()}")
-                }
 
-            } catch (e: Exception) {
-                e.printStackTrace()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
-
     }
 }
--- app/src/main/java/com/milosev/googlemapstestsandbox/MainActivity.kt
+++ app/src/main/java/com/milosev/googlemapstestsandbox/MainActivity.kt
@@ -2,15 +2,19 @@ package com.milosev.googlemapstestsandbox
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.milosev.googlemapstestsandbox.databinding.ActivityMainBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private var kmlUpdateJob: Job? = null
     private lateinit var binding: ActivityMainBinding
     private lateinit var map: GoogleMap
 
@@ -24,16 +28,31 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        val mainActivity = this;
+        var isStarted = false;
+
         binding.btnLoadKml.setOnClickListener {
-            val loadKml = LoadKml()
-            loadKml.execute(this, map)
+            isStarted = !isStarted
+
+            if (isStarted) {
+                binding.btnLoadKml.text = "Stop"
+                val loadKml = LoadKml()
+                kmlUpdateJob = lifecycleScope.launch {
+                    loadKml.execute(mainActivity, map, binding.etKmlUrl.text.toString())
+                }
+            }
+            else {
+                kmlUpdateJob?.cancel()
+                kmlUpdateJob = null
+                binding.btnLoadKml.text = "Start"
+            }
         }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        val tunis = LatLng(36.8065, 10.1815)
+        val tunis = LatLng(35.7607919, 10.7537573)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(tunis, 8f))
     }
 }
